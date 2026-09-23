6ba190d249ac07fd7fd7db7290a2ed874f7b829d
techbyshar <techbyshar.net@gmail.com>
2026-09-22 23:01:56 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach((link) => {
-  link.addEventListener('click', () => document.body.classList.remove('menu-open'));
-});
-
-const year = document.querySelector('#year');
-if (year) year.textContent = new Date().getFullYear();
